0d601564881d6823200ea2afb12af6f2c6f57e25
Deepankar20 <deepankardt2012@gmail.com>
2025-06-20 10:52:09 +0530

    change db file

--- server/src/db/db.ts
+++ server/src/db/db.ts
@@ -1,12 +1,5 @@
-// src/lib/prisma.ts (or just prisma.ts)
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
-};
-
-const prisma = globalForPrisma.prisma ?? new PrismaClient();
-
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+const prisma = new PrismaClient();
 
 export default prisma;
